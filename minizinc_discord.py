6c2570080dfcb7a7e8c69b242326278419caa0f3
Jip J. Dekker <jip@dekker.one>
2020-04-06 15:18:34 +1000

    Fix formatting

--- minizinc_discord.py
+++ minizinc_discord.py
@@ -8,9 +8,9 @@ import minizinc
 from discord.ext import commands
 
 # Initialise Discord bot
-TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!')
-running_messages = ([
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix="!")
+running_messages = [
     "Commencing infinite loop...",
     "The Constraint Elders are contemplating your request...",
     "Generating random numbers ...er... I mean solving! Yeah, solving...",
@@ -20,11 +20,12 @@ running_messages = ([
     "Constructing additional pylons...",
     "Proving P=NP...",
     "Work, work...",
-])
+]
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f"{bot.user} has connected to Discord!")
 
 
 # Initialise MiniZinc
@@ -33,13 +34,13 @@ no_solver = minizinc.Solver("No Solver", "1.0.0", "com.discord.no_solver", "fals
 
 
 # MiniZinc commands
-@bot.command(name='version', help='Report the current MiniZinc version')
+@bot.command(name="version", help="Report the current MiniZinc version")
 async def version(ctx):
     response = minizinc.default_driver.minizinc_version
     await ctx.send(response)
 
 
-@bot.command(name='mzn', help='Run a MiniZinc instance')
+@bot.command(name="mzn", help="Run a MiniZinc instance")
 async def mzn(ctx, *, arg: str):
     response = await ctx.send(random.choice(running_messages))
 
@@ -56,7 +57,7 @@ async def mzn(ctx, *, arg: str):
     await response.edit(content=output)
 
 
-@bot.command(name='flatten', help='Flatten a MiniZinc instance')
+@bot.command(name="flatten", help="Flatten a MiniZinc instance")
 async def mzn(ctx, *, arg: str):
     response = await ctx.send(random.choice(running_messages))
 
