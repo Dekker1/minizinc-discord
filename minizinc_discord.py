ab094d58e7db75812e54e9deaf08ad046b2b03a4
Jip J. Dekker <jip@dekker.one>
2020-04-06 14:41:48 +1000

    Add flatten command

--- minizinc_discord.py
+++ minizinc_discord.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from pathlib import Path
 
 import minizinc
 
@@ -17,6 +18,7 @@ async def on_ready():
 
 # Initialise MiniZinc
 chuffed = minizinc.Solver.lookup("chuffed")
+no_solver = minizinc.Solver("No Solver", "1.0.0", "com.discord.no_solver", "false")
 
 
 # MiniZinc commands
@@ -39,6 +41,26 @@ async def mzn(ctx, instance: str):
     try:
         result = await instance.solve_async(timeout=timedelta(seconds=10))
         response = f"`{result.status}` in {result.statistics['time'].total_seconds()}s: {'No Solution' if result.solution is None else str(result.solution)}"
+    except minizinc.MiniZincError as err:
+        response = "```" + str(err) + "```"
+
+    await ctx.send(response)
+
+
+@bot.command(name='flatten', help='Flatten a MiniZinc instance')
+async def mzn(ctx, instance: str):
+    message = ctx.message.content
+    assert message.startswith("!flatten ")
+    message = message.replace("!flatten ", "", 1)
+    message = message.strip("` \t")
+
+    instance = minizinc.Instance(no_solver)
+    instance.add_string(message)
+
+    try:
+        with instance.flat() as (fzn, ozn, statistics):
+            flatzinc = Path(fzn.name).read_text()
+            response = f"```{flatzinc}```"
     except minizinc.MiniZincError as err:
         response = "```" + str(err) + "```"
 
