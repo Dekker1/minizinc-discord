6e10561ee2b9f6954ac08e47ac023f6045968205
Jip J. Dekker <jip@dekker.one>
2022-11-01 10:54:10 +1100

    Move Discord interaction into contact menus (and slash commands)

--- minizinc_discord.py
+++ minizinc_discord.py
@@ -1,86 +1,11 @@
-from __future__ import annotations
-
+import enum
 import os
-import random
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 import minizinc
-
-import discord
-from discord.ext import commands
-
-# Initialise Discord bot
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(intents=intents, command_prefix="!")
-running_messages = [
-    "Commencing infinite loop...",
-    "The Constraint Elders are contemplating your request...",
-    "Generating random numbers ...er... I mean solving! Yeah, solving...",
-    "Sacrificing a resistor to the machine gods...",
-    "Programming the flux capacitor...",
-    "Compiling Gecode. Sit back and relax...",
-    "Constructing additional pylons...",
-    "Proving P=NP...",
-    "Work, work...",
-    "Facticulating...",
-    "Detonating in 3... 2... 1. Boom town.",
-]
-
-
-class OptionError(Exception):
-    pass
-
-
-class Option:
-    __slots__ = ("key", "value")
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    @classmethod
-    async def convert(cls, ctx, argument):
-        arguments = argument.split("=")
-        if len(arguments) != 2:
-            raise commands.BadArgument("Invalid option")
-
-        return cls(arguments[0], arguments[1])
-
-    @staticmethod
-    def process(options: List[Option], defaults: Dict[str, Any]) -> Dict[str, Any]:
-        ret = defaults.copy()
-
-        for option in options:
-            if option.key == "solver":
-                try:
-                    ret["solver"] = minizinc.Solver.lookup(option.value)
-                except LookupError:
-                    raise OptionError(f'Solver "{option.value}" is not available')
-            elif option.key == "timeout":
-                if int(option.value) <= 30:
-                    ret["timeout"] = timedelta(seconds=int(option.value))
-                else:
-                    raise OptionError(
-                        "Timeout cannot be set to a value higher than 30 seconds"
-                    )
-
-        return ret
-
-    def __repr__(self):
-        return f'"{self.key}"="{self.value}"'
-
-
-@bot.event
-async def on_ready():
-    print(
-        f"{datetime.now().strftime('%H:%M:%S')} - {bot.user} has connected to Discord!"
-    )
+from discord import Intents, Interaction, Message, app_commands, Client, ui
 
 
 def get_time_str(statistics: Dict[str, Any]) -> str:
@@ -98,67 +23,157 @@ def get_time_str(statistics: Dict[str, Any]) -> str:
 
 
 # Initialise MiniZinc
-chuffed = minizinc.Solver.lookup("chuffed")
 no_solver = minizinc.Solver("No Solver", "1.0.0", "com.discord.no_solver", "false")
 
+# Common functions
+async def solve(
+    interaction: Interaction,
+    code: str,
+    solver: minizinc.Solver,
+    time_limit: int,
+):
+    await interaction.response.defer(thinking=True)
 
-# MiniZinc commands
-@bot.command(name="version", help="Report the current MiniZinc version")
-async def version(ctx: commands.Context):
-    response = minizinc.default_driver.minizinc_version
-    await ctx.reply(response)
-
-
-@bot.command(name="mzn", help="Run a MiniZinc instance")
-async def mzn(ctx: commands.Context, options: commands.Greedy[Option], *, arg: str):
-    response = await ctx.reply(random.choice(running_messages))
-    await ctx.message.add_reaction("⌛")
-
-    arg = arg.strip("` \t")
-
-    arguments = {
-        "solver": chuffed,
-        "timeout": timedelta(seconds=30),
-    }
+    code = code.strip("` \t")
+    time_limit = timedelta(seconds=time_limit)
 
     try:
-        arguments = Option.process(options, arguments)
-        instance = minizinc.Instance(arguments["solver"])
-        instance.add_string(arg)
-        result = await instance.solve_async(timeout=arguments["timeout"])
-        output = f"`{result.status}` in {get_time_str(result.statistics)}: {'```' + str(result.solution) + '```' if result.solution is not None else 'No Solution'}"
-    except (OptionError, minizinc.MiniZincError) as err:
-        output = "```" + str(err) + "```"
+        instance = minizinc.Instance(solver)
+        instance.add_string(code)
+        result = await instance.solve_async(timeout=time_limit)
+        sol = str(result.solution) if result.solution is not None else "No Solution"
+        if len(sol) > 1800:
+            sol = sol[:1800]
+            sol += "\n% ...TRUNCATED..."
+        await interaction.followup.send(
+            f"{solver.name}, version {solver.version}, reported `{result.status}` in {get_time_str(result.statistics)}:```{sol}```",
+        )
+    except minizinc.MiniZincError as err:
+        await interaction.followup.send(f"```{str(err)}```")
 
-    await response.edit(content=output)
-    await ctx.message.remove_reaction("⌛", bot.user)
-    await ctx.message.add_reaction("✅")
 
+async def flatten(
+    interaction: Interaction, code: str, solver: minizinc.Solver, time_limit: int
+):
+    await interaction.response.defer(thinking=True)
 
-@bot.command(name="flatten", help="Flatten a MiniZinc instance")
-async def flatten(ctx: commands.Context, options: commands.Greedy[Option], *, arg: str):
-    response = await ctx.reply(random.choice(running_messages))
-    await ctx.message.add_reaction("⌛")
+    code = code.strip("` \t")
+    time_limit = timedelta(seconds=time_limit)
 
-    arg = arg.strip("` \t")
-
-    arguments = {
-        "solver": no_solver,
-        "timeout": timedelta(seconds=30),
-    }
     try:
-        arguments = Option.process(options, arguments)
-        instance = minizinc.Instance(arguments["solver"])
-        instance.add_string(arg)
-        with instance.flat(arguments["timeout"]) as (fzn, ozn, statistics):
+        instance = minizinc.Instance(solver)
+        instance.add_string(code)
+        with instance.flat(timeout=time_limit) as (fzn, ozn, statistics):
             flatzinc = Path(fzn.name).read_text()
-            output = f"```{flatzinc}```"
-    except (OptionError, minizinc.MiniZincError) as err:
-        output = "```" + str(err) + "```"
+            if len(flatzinc) > 1800:
+                flatzinc = flatzinc[:1800]
+                flatzinc += "\n% ...TRUNCATED..."
+            await interaction.followup.send(
+                f"{solver.name}, version {solver.version}, would be given the following FlatZinc:```{flatzinc}```"
+            )
+            # FIXME: Full FlatZinc should be attached as a file when exceeding
+    except minizinc.MiniZincError as err:
+        await interaction.followup.send(f"```{str(err)}```")
 
-    await response.edit(content=output)
-    await ctx.message.remove_reaction("⌛", bot.user)
-    await ctx.message.add_reaction("✅")
+
+class MZNAction(enum.Enum):
+    SOLVE = enum.auto()
+    FLATTEN = enum.auto()
 
 
-bot.run(TOKEN)
+class OptionModal(ui.Modal):
+    solver = ui.TextInput(label="Solver")
+    # solver = ui.Select(options=["cbc", "chuffed", "gecode"], placeholder="chuffed")
+    time_limit = ui.TextInput(label="Time Limit", default="15")
+
+    def __init__(self, message: Message, action: MZNAction) -> None:
+        self.message = message
+        self.action = action
+        if action == MZNAction.SOLVE:
+            title = "MiniZinc Solve Options"
+            self.solver.default = "gecode"
+        else:
+            title = "MiniZinc Flatten Options"
+            self.solver.default = "stdlib"
+        super().__init__(title=title)
+
+    async def on_submit(self, interaction: Interaction):
+        time_limit = 15
+        try:
+            time_limit = int(self.time_limit.value)
+            if time_limit > 30:
+                await interaction.response.send_message(
+                    f"time limit cannot be set to more than 30 seconds", ephemeral=True
+                )
+        except ValueError:
+            await interaction.response.send_message(
+                f"expected integer time limit, received {self.time_limit.value}",
+                ephemeral=True,
+            )
+
+        try:
+            if self.action == MZNAction.FLATTEN and self.solver.value == "stdlib":
+                solver = no_solver
+            else:
+                solver = minizinc.Solver.lookup(self.solver.value)
+        except LookupError:
+            await interaction.response.send_message(
+                f"solver {self.time_limit.value} could not be found", ephemeral=True
+            )
+
+        if self.action == MZNAction.SOLVE:
+            await solve(interaction, self.message.content, solver, time_limit)
+        else:
+            await flatten(interaction, self.message.content, solver, time_limit)
+
+
+@app_commands.context_menu(name="Solve MiniZinc")
+async def solve_menu(interaction: Interaction, message: Message):
+    await interaction.response.send_modal(OptionModal(message, MZNAction.SOLVE))
+
+
+@app_commands.context_menu(name="Flatten MiniZinc")
+async def flatten_menu(interaction: Interaction, message: Message):
+    await interaction.response.send_modal(OptionModal(message, MZNAction.FLATTEN))
+
+
+# A command group that combines the MiniZinc commands
+class MZN(app_commands.Group):
+    def __init__(self):
+        super().__init__(name="mzn")
+
+    @app_commands.command(description="Get the version of the MiniZinc compiler")
+    @app_commands.describe(announce="Send version response to everyone in the channel")
+    async def version(self, interaction: Interaction, announce: bool = False) -> None:
+        response = minizinc.default_driver.minizinc_version
+        await interaction.response.send_message(response, ephemeral=not announce)
+
+
+class MZNClient(Client):
+    def __init__(self):
+        # Initialise super
+        intents = Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
+
+        # Create tree object to add commands to
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.add_command(MZN())
+        self.tree.add_command(solve_menu)
+        self.tree.add_command(flatten_menu)
+        await self.tree.sync()
+
+    async def on_ready(self):
+        print(
+            f"{datetime.now().strftime('%H:%M:%S')} - {self.user} has connected to Discord!"
+        )
+
+
+if __name__ == "__main__":
+    # Initialise Discord bot
+    TOKEN = os.getenv("DISCORD_TOKEN")
+
+    client = MZNClient()
+    client.run(TOKEN)
